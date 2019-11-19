cd7f1004a79e4362f60ef4cf523a9ee966997f01
AlexandarTheCoder <aceivanov0@gmail.com>
2019-11-19 14:50:17 +0100

    Add files via upload
    Start of the project.

---
+++ TestAppSolution/TestApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task< IActionResult> Privacy()
+        {
+            HttpClient _client = new HttpClient();
+
+            var httpResponse = await _client.GetAsync(@"https://localhost:5012/weatherforecast");
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+
+            var tasks = JsonConvert.DeserializeObject<List<Weatherforecast>>(content);
+
+            return View(tasks);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+
+
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            
+        }
+    }
+}
---
+++ TestAppSolution/TestApp/Models/Weatherforecast.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class Weatherforecast
+    {
+        [JsonProperty("date")]
+        public DateTime Date { get; set; } 
+        
+        [JsonProperty("temperatureC")]
+        public int TemperatureC { get; set; } 
+        
+        [JsonProperty("temperatureF")]
+        public int TemperatureF { get; set; }  
+        
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+    }
+}
